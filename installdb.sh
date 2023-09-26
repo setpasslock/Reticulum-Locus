75271a6fd4b113ed0dee1ea6d7df380b6bbab60c
Yusuf D <92121578+setpasslock@users.noreply.github.com>
2023-09-26 21:02:20 +0300

    Update installdb.sh

--- installdb.sh
+++ installdb.sh
@@ -4,6 +4,6 @@ unzip ip2location.zip;
 cd ip2location/;
 chown mysql:mysql db.opt ip2location_db11.frm  ip2location_db11.MYD  ip2location_db11.MYI;
 cd ..; chown mysql:mysql ip2location
-cp -r ip2location /var/lib/mysql/;
+sudo cp -p -r ip2location /var/lib/mysql/;
 sudo systemctl restart mariadb.service;
 echo "Database is ready";
