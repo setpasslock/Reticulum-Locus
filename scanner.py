0c8c6d126f8c7531505f781374add8a18113d47a
Yusuf D <92121578+setpasslock@users.noreply.github.com>
2023-12-02 16:24:08 +0300

    Update scanner.py
    output is saved as ip:port

--- scanner.py
+++ scanner.py
@@ -14,18 +14,38 @@ def RustScan(target_ports=None, city_name=None, first_index=None, second_index=N
     print(f"number of hosts to process {hosts_num_final}")
     result = os.popen(f"sudo rustscan -a 'hosts.txt' -p {target_ports} -b 1000 -g").read()
     print(result)
+    print("new result -------------------------------")
+    result_dict = {}
+    result = result.replace("->",":")
+    #print(result)
+    
+    lines = result.split('\n')
 
+    for line in lines:
+        if line:
+            parts = line.split(' : ')
+            ip = parts[0].strip()
+            ports = [int(port.strip()) for port in parts[1].strip('[]').split(',')]
+            result_dict[ip] = ports
 
-
+    #print(result_dict)
     if result != "":
         if index == None:
             with open(f'{city_name}-{first_index}-{second_index}-results_hosts.txt', mode='w') as new_file:
-                new_file.write(str(result) + '\n')
+                for ip, ports in result_dict.items():
+                    for port in ports:
+                        result = new_file.write(f"{ip}:{port}\n")
         else:
             with open(f'{city_name}-{index}-results_hosts.txt', mode='w') as new_file:
-                    new_file.write(str(result) + '\n')
-
+                for ip, ports in result_dict.items():
+                    for port in ports:
+                        result = new_file.write(f"{ip}:{port}\n")
         print("Results are saved")
     else:
         print("Empty Results")
     return result
+
+
+
+
+   
