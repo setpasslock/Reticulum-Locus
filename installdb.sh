a976002e0ed332688448e61c18f7548abe3bfda8
Yusuf D <92121578+setpasslock@users.noreply.github.com>
2025-02-18 14:29:23 +0300

    Delete installdb.sh

--- installdb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-unzip ip2location.zip;
-cd ip2location/;
-chown mysql:mysql db.opt ip2location_db11.frm  ip2location_db11.MYD  ip2location_db11.MYI;
-cd ..; chown mysql:mysql ip2location
-sudo cp -p -r ip2location /var/lib/mysql/;
-sudo systemctl restart mariadb.service;
-echo "Database is ready";
